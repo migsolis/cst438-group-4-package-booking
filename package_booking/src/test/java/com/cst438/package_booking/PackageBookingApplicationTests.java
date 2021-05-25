42e1f841fbde5cbdebca546364d24776b3cdde77
msolis <msolis@192.168.2.7>
2021-05-25 12:09:39 -0700

    Added empty spring boot project

---
+++ package_booking/src/test/java/com/cst438/package_booking/PackageBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cst438.package_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
